a61a7d7772e8b8c0890f12e7aecc7255de7968a8
@NicBeans <nicholasbosman007@gmail.com>
2023-06-01 15:22:14 +0200

    Initial commit

---
+++ Database.php
@@ -0,0 +1,14 @@
+public function __construct()
+    {
+        $dsn= 'mysql:host=' . $this->host . ';port=' . $this->port . ';dbname=' . $this->dbname . ';sslmode=' . $this->ssl;
+        $options = [
+            PDO::ATTR_PERSISTENT => true,
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        ];
+
+        try {
+            $this->dbh = new PDO($dsn, $this->user, $this->password, $options);
+        } catch (PDOException $e) {
+            $_SESSION['error'] = $e->getMessage();
+        }
+    }
