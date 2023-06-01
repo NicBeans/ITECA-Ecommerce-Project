2a7fe5f2eacf925330f3c5992ad91fd1473274de
@NicBeans <nicholasbosman007@gmail.com>
2023-06-01 18:19:36 +0400

    Work on routing and controllers

---
+++ public/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<!-- get google fonts -->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;400&display=swap" rel="stylesheet">
+<body>
+    <div class="header">
+    <div class="container">
+        <div class="navbar">
+            <div class="logo">
+                <img src="images/logo.jpg" alt="logo" width="200px">
+            </div>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="#">Gallery</a></li>
+                    <li><a href="#">Me</a></li>
+                </ul>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col">
+            <h1>Hi, I'm <span>Sebastian Kornel</span></h1>
+            <p>I create art. <br>A new type of art.</p>
+            <h2>Digital Art involving Artificial Intelligence</h2>
+            <a href="#" class="gallery-btn">Visit my gallery &#8594;</a>
+        </div>
+        <div class="col">
+            <div class="hero-img">
+                <img src="images/printout_poster1.png" alt="Printouts of my art">
+            </div>
+        </div>
+    </div>
+
+    <!-- Featured items -->
+    <div class = "featured-items">
+        <h2>Featured Items</h2>
+        <div class="row">
+            <div class="col-1">
+                <img src="images/astronaut1.png" alt="featured item 1">
+            </div>
+            <div class="col-1">
+                <img src="images/landscape1.png" alt="featured item 2">
+            </div>
+            <div class="col-1">
+                <img src="images/space_kitten1.png" alt="featured item 3">
+            </div>
+        </div>
+
+</div>
+</body>
+</html>
+
